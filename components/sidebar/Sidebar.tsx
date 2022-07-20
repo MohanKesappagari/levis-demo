d66b6c9d8ce250cfe4b394346b78b5451e7d62d5
mohankesappagari <mohan@piktorlabs.com>
2022-07-20 22:08:55 +0530

    p

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -20,8 +20,8 @@ export default function Sidebar({ id }: Props) {
     }
   }, [product]);
   return (
-    <div className="w-2/6">
-      <div className="w-full h-1/6 text-center ">
+    <div className="w-full sm:w-[25%]">
+      <div className="w-full h-full sm:h-[12%] text-center ">
         <Image
           alt="logo"
           src="/assets/VERA_Images/levis-white 1@3x.png"
@@ -29,28 +29,45 @@ export default function Sidebar({ id }: Props) {
           height={70}
         />
       </div>
-      <div className="flex flex-col bg-red h-5/6 items-center ">
-        <div className="mt-5 text-center">
-          <Image
-            src={product[0].raw_data.img_url}
-            alt={STRING(product[0].page_url)}
-            width={240}
-            height={180}
-            objectFit="cover"
-            className="rounded-3xl w-full h-full"
-          />
+      <div className="flex flex-col px-6 sm:px-0  bg-red  sm:h-[88%]  ">
+        <div className="flex flex-row sm:flex-col">
+          <div className="mt-5 text-center">
+            <Image
+              src={product[0].raw_data.img_url}
+              alt={STRING(product[0].page_url)}
+              width={250}
+              height={230}
+              objectFit="cover"
+              className="rounded-3xl"
+            />
+          </div>
+          <div className=" text-white w-10/12 pl-20 sm:pl-10 space-y-2 mt-2">
+            <p className="text-base sm:text-xl font-medium">
+              {STRING(product[0].page_url)}
+            </p>
+            <p className="">
+              Product Code: <strong>{product[0].raw_data.id}</strong>{" "}
+            </p>
+            <p className="">
+              Color: <strong>{color}</strong>{" "}
+            </p>
+            <p className="">
+              Size: <strong>M</strong>{" "}
+            </p>
+          </div>
         </div>
-        <div className=" text-white w-10/12 pl-4 space-y-2 mt-2">
-          <p className=" text-xl font-medium">{STRING(product[0].page_url)}</p>
-          <p className="">
-            Product Code: <strong>{product[0].raw_data.id}</strong>{" "}
-          </p>
-          <p className="">
-            Color: <strong>{color}</strong>{" "}
-          </p>
-          <p className="">
-            Size: <strong>M</strong>{" "}
-          </p>
+        <div className="mb-4 font-medium">
+          <div className="flex w-full mt-6 mb-5 text-white justify-evenly">
+            <button className="border px-4 py-1 border-white border-solid hover:transform hover:scale-110	hover:border-2">
+              Add To Cart
+            </button>
+            <button className="border px-4 py-1 border-white text-red bg-white border-solid hover:transform hover:scale-110">
+              Buy Now
+            </button>
+          </div>
+          <div className="w-full flex items-center justify-center text-white hover:transform hover:scale-110">
+            <button className="border px-4 py-1">Exit</button>
+          </div>
         </div>
       </div>
     </div>
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -3,9 +3,14 @@ import { DEMO } from "../../global/constants/demo";
 
 export default function Product({ id }: any) {
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-screen h-full sm:h-screen sm:flex">
       <Sidebar id={id} />
-      <div className="w-2/3 h-full bg-blue-600"></div>
+      <div className="w-full sm:w-[75%] h-full bg-blue-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius odit
+        similique dolore reiciendis repudiandae quisquam ducimus dignissimos
+        earum placeat tempore suscipit accusamus obcaecati rem adipisci,
+        delectus eligendi cum nisi perspiciatis.
+      </div>
     </div>
   );
 }
