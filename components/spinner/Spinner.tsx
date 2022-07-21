43da51a10b76aa346d6c4e59e0168b839aea25ac
mohankesappagari <mohan@piktorlabs.com>
2022-07-21 17:15:14 +0530

    p

--- components/spinner/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { DotLoader } from "react-spinners";
-import "./spinner.css";
-export default function Spinner() {
-  return (
-    <div className="spinner">
-      <DotLoader color="#c41230" />
-    </div>
-  );
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  <Head>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-      rel="stylesheet"
-    />
-  </Head>;
   return <Component {...pageProps} />;
 }
 
